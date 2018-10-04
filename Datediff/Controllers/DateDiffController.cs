59a609fbf9f63eeab09953c268fe8ac14289c8b1
Maduri <Ramarao.Maduri@wellsfargo.com>
2018-10-04 16:19:01 +0530

    First commit

---
+++ Datediff/Controllers/DateDiffController.cs
@@ -0,0 +1,31 @@
+﻿using Datediff.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Datediff.Controllers
+{
+    public class DateDiffController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult OnSubmit(DateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // do your stuff like: save to database and redirect to required page.
+                
+            }
+            return View("Index", model);
+
+            // If we got this far, something failed, redisplay form
+
+        }
+    }
+}
